2eefc678e194165685a0b54b6cf6199816ba241e
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-11-30 15:51:34 -0300

    Update mux2x1.vhd

--- dsg/Datapath/mux2x1.vhd
+++ dsg/Datapath/mux2x1.vhd
@@ -1,1 +1,27 @@
+library ieee;
 
+use ieee.std_logic_1164.all;
+
+entity mux2x1 is
+        port
+        (
+          -- Input ports
+          A, B : in std_logic_vector(15 downto 0);
+          sel : in std_logic; -- Sel_5
+
+          -- Output ports
+          S : out std_logic_vector(15 downto 0)
+        );
+end mux2x1;
+  
+architecture behaviour of mux2x1 is
+begin
+  process (A, B, S)
+    begin
+      if sel = ‘0’ then
+        S <= A;
+      else
+        s <= B;
+      end if;
+  end process;
+end behaviour;
