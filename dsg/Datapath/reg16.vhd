3a76f22e474728356c6e0c2cae51441522e89477
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2022-11-30 16:28:46 -0300

    Update reg16.vhd

--- dsg/Datapath/reg16.vhd
+++ dsg/Datapath/reg16.vhd
@@ -7,9 +7,8 @@ port(
 
 		-- Input Ports 
    clk     : in std_logic;
-	 ld      : in std_logic; -- (ld0, ld1, ld2, ld3, ld4)
+   ld      : in std_logic; -- (ld0, ld1, ld2, ld3, ld4)
    X       : in std_logic_vector(15 downto 0);       
-
 		-- Output Ports
    Y:     out std_logic_vector(15 downto 0)
 
@@ -18,12 +17,12 @@ end Reg16;
 	
 architecture behavioral of Reg16 is
 begin
-	process(clk, ld)
-		begin
-			if(rising_edge(clk) and ld = '1') then
-				Y <= X;
-      else
-        Y <= Y;
-			end if;
-	end process;
+ process(clk, ld)
+  begin
+   if(rising_edge(clk) and ld = '1') then
+     Y <= X;
+   else
+     Y <= Y; -- Corrigir no cyclone II 
+   end if;
+  end process;
 end behavioral;
